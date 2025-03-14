3daf9762d0f37fd7eda4a8381741df8e9259dc04
Harsh Gor <harshmgor@gmail.com>
2025-03-14 18:58:13 +0530

    refact: update portfolio with new logo, personal details, and social links; enhance styling and update dependencies

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,10 +4,11 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
+import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
+import { SocialLinks } from "../../Assets/constants";
 
 function Home2() {
   return (
@@ -19,37 +20,36 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming, and over the years, I've built a strong foundation in various technologies.
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I am fluent in languages like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, JavaScript, and Go, </b>
+              </i>
+              with expertise in
+              <i>
+                <b className="purple"> DevOps </b>
+              </i>
+              practices such as
+              <i>
+                <b className="purple"> AWS, Jenkins, CICD, Kubernetes, and Terraform. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+
+              My professional interests revolve around creating robust products, particularly excelling in developing secure
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
+                <b className="purple"> API-driven solutions and microservices architectures.</b>
               </i>
+
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Whenever possible, I apply my passion for development by integrating backend technologies such as
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
+                <b className="purple"> Python, FastAPI, Django, and Cloud Services </b>
               </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              to build scalable and intuitive applications.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href={SocialLinks.gitHub}
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,17 +77,17 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href={SocialLinks.x}
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
                 >
-                  <AiOutlineTwitter />
+                  <FaXTwitter />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href={SocialLinks.linkedIn}
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href={SocialLinks.instagram}
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
+          "DevOps Engineer",
+          "Cloud Architect",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
